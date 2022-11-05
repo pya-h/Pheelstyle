a1c7dc2d7b7c5b037855743ab6a2661e2fbfb884
pya <p4ya.py@gmail.com>
2022-11-05 16:54:42 +0330

    rating on home page

--- pheelstyle/views.py
+++ pheelstyle/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from store.models import Product
+from store.models import Product, Review
 
 
 def home(request):
-    popular_products = Product.objects.all().filter(available=True)
+    popular_products = Product.objects.all().filter(available=True).order_by('-created')
+
+    for product in popular_products:
+        reviews = Review.objects.filter(product_id=product.id, status=True)
     context = {
-        'popular_products': popular_products
+        'popular_products': popular_products,
+        'reviews': reviews
     }
     return render(request, 'index.html', context)
